250567a61957af2dbcc1c89ecdf66c3b3ab7c390
dalatcoder <hieuntctk42@gmail.com>
2019-08-15 15:16:35 +0700

    Mac dinh nhap mang dau tien

--- Bai-tap-tren-lop/1812756_NguyenTrongHieu_Lab02/1812756_NguyenTrongHieu_Lab02_Bai1/1812756_NguyenTrongHieu_Lab02_Bai1/program.cpp
+++ Bai-tap-tren-lop/1812756_NguyenTrongHieu_Lab02/1812756_NguyenTrongHieu_Lab02_Bai1/1812756_NguyenTrongHieu_Lab02_Bai1/program.cpp
@@ -19,6 +19,14 @@ void ChayChuongTrinh()
 	int a[MAX], n = 0;
 	int menu, soMenu = 6;
 
+	char filename[MAX];
+	cout << "\n1. Doc mang tu file";
+	cout << "\nNhap ten file: ";
+	cin >> filename;
+	docTapTin(filename, a, n);
+	cout << "\nDay so vua doc la: \n";
+	XuatMang(a, n);
+
 	do
 	{
 		menu = ChonMenu(soMenu);
