0c3a7ed9612f21dcb3dcfdbc3cbf3a1e2732c4d9
dalatcoder <hieuntctk42@gmail.com>
2019-08-15 15:12:45 +0700

    Xoa chuc nang tim kiem noi suy

--- Bai-tap-tren-lop/1812756_NguyenTrongHieu_Lab02/1812756_NguyenTrongHieu_Lab02_Bai1/1812756_NguyenTrongHieu_Lab02_Bai1/thuvien.h
+++ Bai-tap-tren-lop/1812756_NguyenTrongHieu_Lab02/1812756_NguyenTrongHieu_Lab02_Bai1/1812756_NguyenTrongHieu_Lab02_Bai1/thuvien.h
@@ -92,22 +92,3 @@ int TimKiemNhiPhan(int a[MAX], int n, int x)
 	return kq; 
 }
 
-int TimNoiSuy(int a[MAX], int n, int x)
-{
-	int left = 0, right = n - 1, mid;
-	while (left <= right)
-	{
-		mid = left + (right - left)*(x - a[left]) / (a[right] - a[left]);
-		if (a[mid] == x)
-			return 1;
-		else
-		{
-			if (a[mid] > x)
-				right = mid - 1;
-			else
-				left = mid + 1;
-		}
-
-	}
-	return 0;
-}
